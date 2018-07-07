8c3a30c2b29191a6605ef395dedc4767f9688d73
Markiyan Hordiyenko <markhordi@gmail.com>
2018-07-07 17:07:30 +0000

    fix

--- js/index.js
+++ js/index.js
@@ -1,19 +1,15 @@
 (function(){
     
-    document.querySelectorAll('.number').forEach( el => el.addEventListener('click', numbersPress));
-    document.querySelectorAll('.oper').forEach( el => el.addEventListener('click', operPress));
+    document.querySelectorAll('.number' '.oper').forEach( el => el.addEventListener('click', numbersAndOperPress));
     document.querySelector('.equals').addEventListener('click', calculatePress);
     document.querySelector('.sqrt').addEventListener('click', sqrtPress);
     document.querySelector('.clear').addEventListener('click', clearDisplay);
     const display = document.querySelector('.display');
     
-    function numbersPress(){
+    function numbersAndOperPress(){
         display.value += event.target.innerText;
     }
     
-    function operPress(){
-        display.value += event.target.innerText;
-    }
     function calculatePress() {
         display.value = eval(display.value);
     }
